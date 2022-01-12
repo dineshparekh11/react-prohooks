2b226c2e06ba5d9e9bfffd7fb7edeb02d35a3c47
Dinesh Parekh <dinesh21045@gmail.com>
2022-01-12 13:09:52 +0530

    Code finished

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React from 'react';
+
+import Main from './components/MainComponents';
+import { BrowserRouter } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Main />
+    </BrowserRouter>
+  );
+}
+
+export default App;
